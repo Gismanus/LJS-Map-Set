9770acc239d7998ca1ebb8b0873f13a1ead2407f
Michail <Gismanus@gmail.com>
2022-10-24 13:05:05 +0300

    task#1

--- js/test.js
+++ js/test.js
@@ -1,2 +1,15 @@
-let str = 'Hello';
-console.log(str);
+let set = new Set();
+let john = {name: 'John'};
+let pete = {name: 'Pete'};
+let mary = {name: 'Mary'};
+
+set.add(john);
+set.add(mary);
+set.add(pete);
+set.add(mary);
+set.add(john);
+
+for (let user of set){
+    console.log(user.name);
+}
+
